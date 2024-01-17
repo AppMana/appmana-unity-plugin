9efd90bae6f8c85056cca77720bb7ee04e204e4f
doctorpangloss <@hiddenswitch.com>
2024-01-17 14:03:57 -0800

    wip

---
+++ AppManaPublic/XR/XRScaled.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace AppMana.XR
+{
+    internal class XRScaled : UIBehaviour
+    {
+        public float scale
+        {
+            set => transform.localScale = value * Vector3.one;
+        }
+    }
+}
